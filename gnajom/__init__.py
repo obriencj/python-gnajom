ebfaad94db0351405e26cad3da6f8918f9b788db
Christopher O'Brien <obriencj@gmail.com>
2014-09-13 15:09:28 -0400

    import

---
+++ gnajom/__init__.py
@@ -0,0 +1,227 @@
+# This library is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as
+# published by the Free Software Foundation; either version 3 of the
+# License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, see
+# <http://www.gnu.org/licenses/>.
+
+
+"""
+gnajom - Python package for working with Mojang API services.
+
+The core module focuses on interacting with Yggdrasil, Mojang's
+authentication system.
+
+:author: Christopher O'Brien <obriencj@gmail.com>
+:license: LGPL v3
+"""
+
+
+from json import dumps
+from optparse import OptionParser, OptionGroup
+from os.path import expanduser
+from requests import post
+from requests.exceptions import HTTPError
+from uuid import uuid1
+
+import sys
+
+
+__all__ = ( "ApiObject", "Authentication",
+            "cli", "cli_options", "cli_auth_optgroup",
+            "cli_do_authenticate", "cli_do_refresh",
+            "cli_do_validate", "cli_do_invalidate",
+            "cli_do_signoff",
+            "main" )
+
+
+HOST_YGGDRASIL = "https://authserver.mojang.com"
+
+
+MINECRAFT_AGENT_V1 = {
+    "name": "Minecraft",
+    "version": 1,
+}
+
+
+class ApiObject(object):
+    """
+    Lightweight wrapper for JSON via POST
+    """
+
+    def __init__(self, hosturi):
+        self._host = hosturi
+
+
+    def post(self, endpoint, payload):
+        data = dumps(payload)
+        resp = post(self._host + endpoint, data)
+        resp.raise_for_status()
+        return resp.json()
+
+
+class Authentication(ApiObject):
+    """
+    A thin wrapper for the Mojang authentiation scheme, 'Yggdrasil'
+
+    References
+    ----------
+    * http://wiki.vg/Authentication
+    """
+
+    def __init__(self, username, clientToken=None, accessToken=None,
+                 authhost=HOST_YGGDRASIL, agent=MINECRAFT_AGENT_V1):
+
+        self.api = ApiObject(authhost)
+        self.username = username
+        self.agent = agent
+        self.clientToken = None
+        self.accessToken = None
+        self.profile = None
+
+
+    def authenticate(self, password):
+        payload = { "username": self.username,
+                    "password": password, }
+
+        if self.agent:
+            payload["agent"] = self.agent
+
+        if self.clientToken:
+            payload["clientToken"] = self.clientToken
+
+        ret = self.api.post("/authenticate", payload)
+
+        self.clientToken = ret["clientToken"]
+        self.accessToken = ret["accessToken"]
+        self.profile = ret["selectedProfile"]
+
+
+    def refresh(self):
+        payload = { "accessToken": self.accessToken,
+                    "clientToken": self.clientToken,
+                    "selectedProfile": self.profile, }
+
+        ret = self.api.post("/refresh", payload)
+
+
+    def validate(self):
+        payload = { "accessToken": self.accessToken, }
+
+        ret = self.api.post("/validate", payload)
+
+
+    def signout(self, password):
+        payload = { "username": self.username,
+                    "password": password, }
+
+        ret = self.api.post("/signout", payload)
+
+
+    def invalidate(self):
+        payload = { "accessToken": self.accessToken,
+                    "clientToken": self.clientToken, }
+
+        ret = self.api.post("/invalidate", payload)
+
+
+def gen_clientToken():
+    """
+    Generate a random clientToken string via UUID
+    """
+
+    return uuid.uuid1().bytes.encode("hex")
+
+
+def cli_do_authenticate(parser, options, *args):
+    pass
+
+
+def cli_do_refresh(parser, options, *args):
+    pass
+
+
+def cli_do_validate(parser, options, *args):
+    pass
+
+
+def cli_do_signout(parser, options, *args):
+    pass
+
+
+def cli_do_invalidate(parser, options, *args):
+    pass
+
+
+_COMMANDS = {
+    "authenticate": cli_do_authenticate,
+    "refresh": cli_do_refresh,
+    "validate": cli_do_validate,
+    "signout": cli_do_signout,
+    "invalidate": cli_do_invalidate,
+}
+
+
+def cli(parser, options, *args):
+    if len(args) == 0:
+        parser.error("Please specify a command:" +
+                     ", ".join(sorted(_COMMANDS.keys())))
+
+    cmd = args[0].lower()
+    if cmd not in _COMMANDS:
+        parser.error("Please select a valid command:" +
+                     ", ".join(sorted(_COMMANDS.keys())))
+
+    else:
+        _COMMANDS[cmd](parser, options)
+
+
+def cli_optparser():
+    p = OptionParser("%prog COMMAND [options]")
+    p.add_option_group(cli_auth_optgroup(p))
+
+    return p
+
+
+def cli_auth_optgroup(parser):
+    g = OptionGroup(parser, "Authentication Options")
+
+    g.add_option("-c", "--config", action="store", default=DEFAULT_CONFIG,
+                 help="Configuration file to use")
+    g.add_option("-r", "--profile", action="store", default=None,
+                 help="Login profile to use")
+    g.add_option("-u", "--username", action="store", default=None,
+                 help="Mojang account user")
+    g.add_option("-p", "--password", action="store", default=None,
+                 help="Mojang account password")
+    g.add_option("--auth-host", action="store", default=CLIENT_HOST,
+                 help="Mojang Realms host")
+
+    return g
+
+
+def main(args):
+    parser = create_options()
+    try:
+        return cli(parser, *parser.parse(args))
+    except KeyboardInterrupt:
+        print >> sys.stderr
+        return 130
+    except HTTPError as he:
+        print >> sys.stderr, he
+        return -1
+
+
+if __name__ == '__main__':
+    sys.exit(main(sys.args))
+
+
+#
+# The end.
---
+++ setup.py
@@ -0,0 +1,60 @@
+#! /usr/bin/env python2
+
+# This library is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as
+# published by the Free Software Foundation; either version 3 of the
+# License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, see
+# <http://www.gnu.org/licenses/>.
+
+
+"""
+gnajom - Python and command-line tools for working with Monjang's
+public APIs.
+
+:author: Christopher O'Brien  <obriencj@gmail.com>
+:license: LGPL v.3
+"""
+
+
+from setuptools import setup
+
+
+setup( name = "gnojam",
+       version = "0.9.0",
+
+       packages = [ "gnajom" ],
+
+       author = "Christopher O'Brien",
+       author_email = "obriencj@gmail.com",
+       url = "https://github.com/obriencj/python-gnajom",
+       license = "GNU Lesser General Public License",
+
+       description = "Module and command line tools for working with"
+       " Mojang's public APIs",
+
+       provides = [ "gnajom" ],
+       requires = [ "requests" ],
+       platforms = [ "python2 >= 2.6" ],
+
+       zip_safe = True,
+
+       classifiers = [ "Environment :: Console",
+                    "Programming Language :: Python :: 2" ],
+
+       entry_points = {
+           "console_scripts": [
+               'mojang-auth = gnojam:main',
+               #'minecraft-realm = gnojam.realms:main',
+               #'minecraft-user = gnojam.users:main',
+           ]} )
+
+#
+# The end.
