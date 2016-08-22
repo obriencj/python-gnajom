f0b44f7de560d4e05c2694265ecbc431eb228ed5
Christopher O'Brien <obriencj@gmail.com>
2016-08-22 00:41:17 -0400

    fleshing out more mojang API calls

--- gnajom/mojang.py
+++ gnajom/mojang.py
@@ -17,7 +17,8 @@ and service status
 """
 
 
-from gnajom import ApiHelper
+from gnajom import APIHost
+from base64 import b64decode
 
 
 DEFAULT_MOJANG_API_HOST = "https://api.mojang.com"
@@ -36,7 +37,7 @@ class MojangAPI(object):
 
     def __init__(self, auth, host=DEFAULT_MOJANG_API_HOST):
         self.auth = auth
-        self.api = ApiObject(host)
+        self.api = APIHost(host)
 
 
     def username_to_uuid(self, username, at_time=0):
@@ -48,8 +49,32 @@ class MojangAPI(object):
         return self.api.get("/user/profiles/%s/names" % uuid)
 
 
-    def playernames_to_uuid(self, playernames):
+    def playernames_to_uuids(self, playernames):
         return self.api.post("/profiles/minecraft", list(playernames))
+
+
+    def change_skin(self, uuid, model, skin_url):
+        pass
+
+
+    def upload_skin(self, uuid, model, skin_blob):
+        pass
+
+
+    def upload_skin_filename(self, uuid, model, skin_filename):
+        pass
+
+
+    def reset_skin(self, uuid):
+        return self.api.delete("/user/profile/%s/skin" % uuid)
+
+
+    def my_user_info(self):
+        return self.api.get("/user")
+
+
+    def statistics(self):
+        return self.api.get("/statistics")
 
 
 class SessionAPI(object):
@@ -63,7 +88,25 @@ class SessionAPI(object):
 
     def __init__(self, auth, host=DEFAULT_MOJANG_SESSION_HOST):
         self.auth = auth
-        self.api = ApiObject(host)
+        self.api = APIHost(host)
+
+
+    def profile_textures(self, uuid):
+        data = self.api.get("/session/minecraft/profile/%s" % uuid)
+
+        if data:
+            props = data.get("properties")
+            if props:
+                val = props.get("value")
+                val = b64decode(val)
+                val = loads(val)
+                props["value"] = val
+
+        return data
+
+
+    def blocked_servers(self):
+        return self.api.get("/blockedservers")
 
 
 class StatusAPI(object):
@@ -77,7 +120,7 @@ class StatusAPI(object):
 
     def __init__(self, auth=None, host=DEFAULT_MOJANG_STATUS_HOST):
         self.auth = auth
-        self.api = ApiObject(host)
+        self.api = APIHost(host)
 
 
     def check(self):
